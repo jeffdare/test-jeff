da76a304184a3b87e78190730c9c08341c1b076d
Jeffrey Dare <jeffdare@casar1.fyre.ibm.com>
2020-04-05 21:24:41 -0700

    const

--- custom/tsai.py
+++ custom/tsai.py
@@ -6,7 +6,7 @@ from sqlalchemy.sql.sqltypes import TIMESTAMP, VARCHAR
 import numpy as np
 import pandas as pd
 
-from iotfunctions.base import BaseTransformer,BaseSimpleAggregator
+from iotfunctions.base import BaseTransformer,BaseComplexAggregator
 from iotfunctions import ui
 
 logger = logging.getLogger(__name__)
