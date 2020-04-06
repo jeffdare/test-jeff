1c538573cf09949c9ec638b5f7977711b9153e0c
Jeffrey Dare <jeffdare@casar1.fyre.ibm.com>
2020-04-05 21:21:44 -0700

    changes

--- scripts/tsaiTest.py
+++ scripts/tsaiTest.py
@@ -40,17 +40,17 @@ This file will be written to the working directory.
 
 '''
 
-from custom.jeffAgg import JeffAgg
-# fn = JeffAgg(
-#     input_items = ['baselinecount'],
-#     output_items = ['mean_baselinecount']
-#               )
-# fn.execute_local_test(db=db,db_schema=db_schema,generate_days=1,to_csv=True)
+from custom.tsai import Sdd
+fn = Sdd(
+    input_items = ['area_column','distance_column'],
+    output_items = ['name']
+              )
+fn.execute_local_test(db=db,db_schema=db_schema,generate_days=1,to_csv=True)
 
 # '''
 # Register function so that you can see it in the UI
 # '''
 # db.unregister_functions(['JeffAgg'])
-db.register_functions([JeffAgg])
+# db.register_functions([JeffAgg])
 
 
