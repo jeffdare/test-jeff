f5864129d0739d3a395c9112f5d98f82256803e2
Jeffrey Dare <jeffdare@casar1.fyre.ibm.com>
2020-04-05 21:22:24 -0700

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jeffTest',
-    version='0.0.5',
+    version='0.1.0',
     packages=find_packages(),
     install_requires=[
         'dill==0.3.0',
